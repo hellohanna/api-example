2344f464026714d0da35b777eac676617ef4e1a9
Hanna Babushkina <anylike90@gmail.com>
2019-01-21 18:04:02 -0800

    Add http basic authentication

--- sample.sql
+++ sample.sql
@@ -2,3 +2,9 @@ CREATE TABLE data(
     id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL
     );
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    login TEXT NOT NULL,
+    password_hash BYTEA NOT NULL
+    );
--- server.py
+++ server.py
@@ -1,10 +1,37 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from random import randint
 from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+from functools import wraps
 
 
 app = Flask(__name__)
 db = SQLAlchemy()
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+
+    sql = "SELECT password_hash FROM users WHERE login = :username"
+    rows = db.session.execute(sql, {'username': username}).fetchall()
+    if not rows:
+        return False
+    password_hash = bytes(rows[0][0])
+    if password_hash != bcrypt.hashpw(password.encode(), password_hash):
+        return False
+    return True
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return 'unauthorized', 401
+        return f(*args, **kwargs)
+    return decorated
 
 
 @app.route('/')
@@ -13,6 +40,7 @@ def hello_world():
 
 
 @app.route('/data', methods = ['POST'])
+@requires_auth
 def post_data():
     """Creates new record in db"""
 
@@ -34,6 +62,7 @@ def post_data():
 
 
 @app.route('/data/<id>', methods = ['GET'])
+@requires_auth
 def get_data(id):
     """Returns name from db by id"""
 
@@ -50,6 +79,7 @@ def get_data(id):
 
 
 @app.route('/data/<id>', methods = ['DELETE'])
+@requires_auth
 def delete_data(id):
     """Delete name from db by id"""
 
@@ -70,6 +100,7 @@ def delete_data(id):
 
 
 @app.route('/data/<id>', methods = ['PUT'])
+@requires_auth
 def put_data(id):
     """Changes name in db by id"""
 
@@ -93,6 +124,26 @@ def put_data(id):
     return 'data changed', 200
 
 
+def create_test_user():
+    """Create test login and password"""
+
+    login = 'test'
+    password = 'password'
+
+    salt = bcrypt.gensalt()
+    password_hash = bcrypt.hashpw(password.encode(), salt)
+
+    sql = "DELETE FROM users WHERE login = :login"
+    db.session.execute(sql, {'login': login})
+
+    sql = """
+    INSERT INTO users (login, password_hash)
+    VALUES (:login, :password_hash)
+    """
+    db.session.execute(sql, {'login': login, 'password_hash': password_hash})
+    db.session.commit()
+
+
 def connect_to_db(app, database_uri="postgresql:///sampledb"):
     """Connect the database to Flask app."""
 
@@ -101,6 +152,8 @@ def connect_to_db(app, database_uri="postgresql:///sampledb"):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
+    create_test_user()
+
 
 if __name__=='__main__':
     connect_to_db(app)
